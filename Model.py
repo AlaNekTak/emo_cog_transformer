945782e1f0031649495cc7f4edb95918ea6fcd0a
nekouvag <nekouvag@usc.edu>
2024-06-26 11:25:54 -0700

    DoubleExpert Model added

--- Model.py
+++ Model.py
@@ -259,3 +259,146 @@ class DynamicSoftGate(nn.Module):
         # Use softmax to normalize weights to sum to 1
         gate_weights = F.softmax(self.gate_layer(x), dim=1)
         return gate_weights
+
+
+
+class DoubleExp_Emotion_Classifier(pl.LightningModule):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.use_input_embeddings = self.config.use_input_embeddings
+        
+        self.pretrained_model = AutoModel.from_pretrained(self.config.model_name, return_dict=True)
+
+        # Appraisal path
+        self.appraisal_hidden = nn.Linear(self.pretrained_model.config.hidden_size, self.pretrained_model.config.hidden_size)
+        self.appraisal_classifier = nn.Linear(self.pretrained_model.config.hidden_size, 1)
+
+        self.emotion_hidden = nn.Linear(2 * self.pretrained_model.config.hidden_size, self.pretrained_model.config.hidden_size) # Emotion classification components
+        
+        self.emotion_classifier = nn.Linear(self.pretrained_model.config.hidden_size, self.config.class_num)
+
+        self.dynamic_gate = DynamicSoftGate(self.pretrained_model.config.hidden_size, 2)
+        # Loss functions
+        self.appraisal_loss_func = nn.MSELoss()
+        self.emotion_loss_func = nn.CrossEntropyLoss()
+        self.dropout = nn.Dropout(self.config.dropout_rate)
+        
+        ## Weight initialization
+        self.save_hyperparameters()
+        self.initialize_weights()
+
+    def forward(self, input_ids=None, attention_mask=None, appraisal_labels=None, emotion_labels=None, inputs_embeds=None):
+        if self.use_input_embeddings:
+            outputs = self.pretrained_model(inputs_embeds=inputs_embeds, attention_mask=attention_mask)
+        else:
+            outputs = self.pretrained_model(input_ids=input_ids, attention_mask=attention_mask)
+
+        mean_last_hidden = torch.mean(outputs.last_hidden_state, 1)
+
+        # Appraisal processing
+        appraisal_output = F.relu(self.appraisal_hidden(mean_last_hidden))
+        appraisal_logits = self.dropout(self.appraisal_classifier(appraisal_output))
+        
+        # Dynamic
+        gate_weights = self.dynamic_gate(mean_last_hidden)  # Shape: [batch_size, n_attributes]
+        concat_layer = torch.cat([
+                                    gate_weights[:, 0].unsqueeze(1) * appraisal_output,
+                                    gate_weights[:, 1].unsqueeze(1) * mean_last_hidden
+                                ], dim=1)
+
+        emotion_pooled = self.dropout(F.relu(self.emotion_hidden(concat_layer)))
+        emotion_logits = self.emotion_classifier(emotion_pooled)
+
+        total_loss = 0
+        loss_dict = {}
+
+        if appraisal_labels is not None and emotion_labels is not None:
+            emotion_labels = emotion_labels.view(-1)
+            assert emotion_logits.shape == (emotion_labels.shape[0],self.config.class_num), "Mismatch in batch size between logits and labels"
+            emotion_loss = self.emotion_loss_func(emotion_logits, emotion_labels)  # Assuming emotion labels are the last
+            appraisal_loss = self.appraisal_loss_func(appraisal_logits.view(-1), appraisal_labels)
+            total_loss += emotion_loss + appraisal_loss
+            loss_dict['emotion'] = emotion_loss.item()
+            loss_dict['appraisals'] = appraisal_loss.item()
+
+        return total_loss, emotion_logits, appraisal_logits, gate_weights, loss_dict
+
+    def training_step(self, batch, batch_index):
+        total_loss, emotion_logits, appraisal_logits,gate_weights, loss_dict = self(**batch)
+        self.log("train_loss", total_loss, prog_bar=True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+        self.log("emotion_loss", loss_dict['emotion'], prog_bar=True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+        
+        for idx, loss in enumerate(loss_dict['appraisals']):
+                self.log(f'appraisal_{idx}_loss', loss, on_step=False, on_epoch=True)
+
+        if self.config.gate_mechanism == 'soft':
+            mean_gate_weights = gate_weights.mean(dim=0)
+            for idx, weight in enumerate(mean_gate_weights):
+                self.log(f'gate_weight_{idx}', weight, on_step=False, on_epoch=True)
+
+        return {"loss": total_loss, 
+                "emotion_logits":emotion_logits,
+                "appraisal_logits":appraisal_logits,
+                "emotion_labels": batch["emotion_labels"], 
+                "appraisal_labels": batch["appraisal_labels"],                
+                "gate_weights": gate_weights
+                }
+    
+    def validation_step(self, batch, batch_index):
+        total_loss, emotion_logits, appraisal_logits, gate_weights, loss_dict= self(**batch)
+        self.log("val_loss", total_loss, prog_bar = True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+        return {"val_loss": total_loss, "emotion_logits":emotion_logits,"appraisal_logits":appraisal_logits,
+                 "emotion_labels": batch["emotion_labels"], "appraisal_labels": batch["appraisal_labels"]}
+
+    def predict_step(self, batch, batch_index):
+        _, emotion_logits, appraisal_logits, gate_weights, _ = self(**batch)
+        return {
+                "emotion_logits":emotion_logits,
+                "appraisal_logits":appraisal_logits,
+                "emotion_labels": batch["emotion_labels"], 
+                "appraisal_labels": batch["appraisal_labels"],
+                "gate_weights": gate_weights
+                }
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.config.lr, weight_decay=self.config.weight_decay)
+        total_steps = self.config.train_size/self.config.train_batch_size
+        warmup_steps = math.floor(total_steps * self.config.warmup)
+        scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
+        return [optimizer],[scheduler]
+
+    def initialize_weights(self):
+        nn.init.xavier_uniform_(self.appraisal_hidden.weight)
+        nn.init.xavier_uniform_(self.appraisal_classifier.weight)
+        nn.init.xavier_uniform_(self.emotion_hidden.weight)
+        nn.init.xavier_uniform_(self.emotion_classifier.weight)
+
+    def on_train_epoch_end_static(self):
+        # Log the gate weights at the end of each epoch
+        if self.config.gate_mechanism == 'hard':
+            weights = F.softmax(self.selection_weights, dim=0).data
+        elif self.config.gate_mechanism == 'soft':
+            # weights = torch.sigmoid(self.gate_weights).data
+            weights = F.softmax(self.gate_weights.squeeze(), dim=0)
+
+        # Use PyTorch Lightning's logger or any other logging mechanism
+        for idx, weight in enumerate(weights.squeeze()):
+            print(f'gate_weight{idx}-{self.config.attributes[idx]}', weight.item())
+            self.log(f'gate_weight_{idx}-{self.config.attributes[idx]}', weight.mean().item(), on_step=False, on_epoch=True)
+    
+    def on_train_epoch_end(self):
+        if self.config.gate_mechanism == 'soft':
+            for idx in range(self.config.n_attributes):
+                # Retrieve each gate weight average logged during the epoch
+                print(f'{self.config.attributes[idx]} weight: ',self.trainer.callback_metrics.get(f'gate_weight_{idx}').cpu().numpy())
+                print(f'{self.config.attributes[idx]} loss: ',self.trainer.callback_metrics.get(f'appraisal_{idx}_loss').cpu().numpy())
+            print('emotion loss: ',self.trainer.callback_metrics.get(f'emotion_loss').cpu().numpy())
+
+    # def on_validation_epoch_end(self):
+    #     if self.config.gate_mechanism == 'soft':
+    #         for idx in range(self.config.n_attributes):
+    #             # Retrieve each gate weight average logged during the epoch
+    #             print(f'{self.config.attributes[idx]} weight: ',self.trainer.callback_metrics.get(f'gate_weight_{idx}').cpu().numpy())
+    #             print(f'{self.config.attributes[idx]} loss: ',self.trainer.callback_metrics.get(f'appraisal_{idx}_loss').cpu().numpy())
+    #         print('emotion loss: ',self.trainer.callback_metrics.get(f'emotion_loss').cpu().numpy())
