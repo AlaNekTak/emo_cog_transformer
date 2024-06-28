66a132caf31798c875d245cf034a64806f7db649
nekouvag <nekouvag@usc.edu>
2024-06-28 15:16:15 -0700

    added the ProbeEmotionClassifier class

--- Model.py
+++ Model.py
@@ -407,3 +407,95 @@ class RunningMaxLossScaler:
 
         # Normalize the current loss by the updated maximum value
         return current_loss / self.max_val
+    
+
+class ProbeEmotionClassifier(pl.LightningModule):
+    '''
+    Designed to output hidden reps for probing
+    
+    '''
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.use_input_embeddings = self.config.use_input_embeddings
+        self.pretrained_model = AutoModel.from_pretrained(self.config.model_name, return_dict=True)
+        self.emotion_hidden = nn.Linear(2 * self.pretrained_model.config.hidden_size, self.pretrained_model.config.hidden_size) # Emotion classification components
+        self.emotion_classifier = nn.Linear(self.pretrained_model.config.hidden_size, self.config.class_num)
+        self.emotion_loss_func = nn.CrossEntropyLoss()
+        self.dropout = nn.Dropout(self.config.dropout_rate)
+
+        ## Weight initialization
+        self.save_hyperparameters()
+        self.initialize_weights()
+
+    def forward(self, input_ids=None, attention_mask=None, appraisal_labels=None, emotion_labels=None, inputs_embeds=None):
+        if self.use_input_embeddings:
+            outputs = self.pretrained_model(inputs_embeds=inputs_embeds, attention_mask=attention_mask)
+        else:
+            outputs = self.pretrained_model(input_ids=input_ids, attention_mask=attention_mask)
+
+        mean_last_hidden = torch.mean(outputs.last_hidden_state, 1)
+
+        emotion_pooled = self.dropout(F.relu(self.emotion_hidden(mean_last_hidden)))
+        emotion_logits = self.emotion_classifier(emotion_pooled)
+
+        total_loss = 0
+        loss_dict = {}
+
+        emotion_labels = emotion_labels.view(-1)
+        assert emotion_logits.shape == (emotion_labels.shape[0],self.config.class_num), "Mismatch in batch size between logits and labels"
+        emotion_loss = self.emotion_loss_func(emotion_logits, emotion_labels)  # Assuming emotion labels are the last
+        total_loss += emotion_loss
+        loss_dict['emotion'] = emotion_loss.item()
+
+        return total_loss, emotion_logits, loss_dict, mean_last_hidden
+
+    def training_step(self, batch, batch_index):
+        total_loss, emotion_logits, loss_dict, mean_last_hidden = self(**batch)
+        self.log("train_loss", total_loss, prog_bar=True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+        self.log("emotion_loss", loss_dict['emotion'], prog_bar=True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+
+        return {
+                "loss": total_loss, 
+                "emotion_logits":emotion_logits,
+                "emotion_labels": batch["emotion_labels"], 
+                "appraisal_labels": batch["appraisal_labels"],  
+                "mean_last_hidden": mean_last_hidden              
+                }
+    
+    def validation_step(self, batch, batch_index):
+        total_loss, emotion_logits, loss_dict, mean_last_hidden= self(**batch)
+        self.log("val_loss", total_loss, prog_bar = True, logger=True, on_step=False, on_epoch=True, sync_dist=True)
+        return {
+                "val_loss": total_loss, 
+                "emotion_logits":emotion_logits,
+                "emotion_labels": batch["emotion_labels"], 
+                "appraisal_labels": batch["appraisal_labels"],
+                "mean_last_hidden": mean_last_hidden              
+                }
+
+    def predict_step(self, batch, batch_index):
+        _, emotion_logits, appraisal_logits, _, mean_last_hidden = self(**batch)
+        return {
+                "emotion_logits":emotion_logits,
+                "appraisal_logits":appraisal_logits,
+                "emotion_labels": batch["emotion_labels"], 
+                "appraisal_labels": batch["appraisal_labels"],
+                "mean_last_hidden": mean_last_hidden              
+                }
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.config.lr, weight_decay=self.config.weight_decay)
+        total_steps = self.config.train_size/self.config.train_batch_size
+        warmup_steps = math.floor(total_steps * self.config.warmup)
+        scheduler = get_cosine_schedule_with_warmup(optimizer, warmup_steps, total_steps)
+        return [optimizer],[scheduler]
+
+    def initialize_weights(self):
+        nn.init.xavier_uniform_(self.appraisal_hidden.weight)
+        nn.init.xavier_uniform_(self.appraisal_classifier.weight)
+        nn.init.xavier_uniform_(self.emotion_hidden.weight)
+        nn.init.xavier_uniform_(self.emotion_classifier.weight)
+
+    def on_train_epoch_end(self):
+        print('emotion loss: ',self.trainer.callback_metrics.get(f'emotion_loss').cpu().numpy())
